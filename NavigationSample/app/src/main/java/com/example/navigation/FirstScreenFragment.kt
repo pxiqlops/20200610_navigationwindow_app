08e1d10b6f736354e358b79d215987ccaa7ea8a2
tatsumi mukai <63081975+pxiqlops@users.noreply.github.com>
2020-06-10 18:21:19 +0900

    0610

---
+++ NavigationSample/app/src/main/java/com/example/navigation/FirstScreenFragment.kt
@@ -0,0 +1,27 @@
+package com.example.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class FirstScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.btn_goto_second).setOnClickListener { v ->
+            Navigation.findNavController(v).navigate(R.id.secondScreenFragment)
+        }
+    }
+}
